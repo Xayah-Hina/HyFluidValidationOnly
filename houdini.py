f4a419cba44e78386c84e0496233a943ca17517e
Xayah <ycxiang14sep@gmail.com>
2025-04-16 19:14:00 +0900

    update export_density_only/houdini

--- houdini.py
+++ houdini.py
@@ -1,6 +1,8 @@
 import hou
 import os
 
+import numpy as np
+
 
 ### Please use Hython instead of common Python
 ### You can find Hython under Houdini Installed dir
@@ -31,3 +33,16 @@ def export_velocity_field(vel, save_path, surname, bbox):
     output_path = os.path.join(save_path, f"{surname}.bgeo.sc")
     geo.saveToFile(output_path)
     print(f"Save {output_path}")
+
+
+if __name__ == "__main__":
+
+    os.makedirs("export", exist_ok=True)
+    for i in range(120):
+        den = np.load(f"den_{i+1:03d}.npy")
+        export_density_field(
+            den=den,
+            save_path="export",
+            surname=f"density_{i+1:03d}",
+            bbox=(0.0, 0.0, 0.0, 0.4909, 0.7363, 0.4909),
+        )
